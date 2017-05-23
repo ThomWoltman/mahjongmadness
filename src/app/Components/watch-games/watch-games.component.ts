3f5ecb82c73c766852ddcee1ab4197ba5e6a6baf
thomwoltman <twoltman@avans.nl>
2017-05-23 15:24:44 +0200

    lobby changes

---
+++ src/app/Components/watch-games/watch-games.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { GameService } from '../../Services/game.service';
+import { Game } from '../../Models/game';
+import { Subscription } from 'rxjs';
+
+@Component({
+    selector: 'watch-games',
+    templateUrl: './watch-games.component.html',
+    styleUrls: ['./watch-games.component.scss']
+})
+export class WatchGamesComponent {
+    constructor(private gameService: GameService) { }
+
+    games: Game[];
+    busy: Subscription;
+
+    getMyGames(): void {
+        this.busy = this.gameService.getWatchGames().subscribe(games => this.games = games);
+    }
+
+    ngOnInit(): void {
+        this.getMyGames();
+    }
+}
