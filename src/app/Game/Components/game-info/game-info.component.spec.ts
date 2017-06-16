8765f2993fe6d71c6b9942a19558506f25d6fbab
glenndewildt <glenndewildt@gmail.com>
2017-06-16 10:59:41 +0200

    game info

--- src/app/Game/Components/game-info/game-info.component.spec.ts
+++ src/app/Game/Components/game-info/game-info.component.spec.ts
@@ -1,14 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By }              from '@angular/platform-browser';
+import { DebugElement }    from '@angular/core';
 import { GameInfoComponent } from './game-info.component';
+import { ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { GameService } from 'app/shared/Services/game.service';
+import { MaterialModule } from '@angular/material';
+import {Subscription} from "rxjs/Subscription";
+
+import {Player} from "../../../Shared/Models/player";
+import {GameModule} from "../game.module";
 
 describe('GameInfoComponent', () => {
   let component: GameInfoComponent;
   let fixture: ComponentFixture<GameInfoComponent>;
+    let de:      DebugElement;
+    let el:      HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GameInfoComponent ]
+        imports:[GameModule,GameService],
+
+        declarations: [ GameInfoComponent ],
+        providers: [
+            { provide: ComponentFixtureAutoDetect, useValue: true }]
     })
     .compileComponents();
   }));
